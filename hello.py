9ebfdbb84085c0e017723e1ec46f9cb9e7383fbf
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-06 19:47:29 +0300

    Start 2.91

--- hello.py
+++ hello.py
@@ -1,10 +1,22 @@
-def test (environ,start_response):
-    data =  "\n".join(environ.get('QUERY_STRING').split("&"))
-    status = "200 OK"
-    response_headers = [
-        ('Content-Type','text/plain')
-    ]
-    start_response(status,response_headers)
-    return iter ([data.encode('utf-8')])
-    #Ввернуть заголовки, только хз как#
+from cgi import parse_qs                                                        
 
+def application(environ, start_response):
+
+	queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	body = ''
+	print(queryString)
+	for key, value in queryString.items() :
+		for element in value :
+			body += key + '='                                       
+			body += element + '\r\n'
+
+	print(body)                                                             
+		
+	status = '200 OK'
+
+	response_headers = [
+		('Content-Type', 'text/plain'),
+	]
+	start_response(status, response_headers)                                    
+
+	return [body] 
