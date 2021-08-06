a4130c8653bc1e3ad3796a83bdb481f7822a046a
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-06 19:28:04 +0300

    Start 2.99

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ def test (environ,start_response):
     status = "200 OK"
     response_headers = [
         ('Content-Type','text/plain')
-        ('Content-Length', str(len(data)))
     ]
     start_response(status,response_headers)
     return iter ([data.encode('utf-8')])
