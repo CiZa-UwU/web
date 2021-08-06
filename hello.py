3a61ed731470826a43e5857828f9c4a14eae52d2
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-06 19:11:44 +0300

    Start 2.8

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 def test (environ,start_response):
+    data =  "\n".join(environ.get('QUERY_STRING').split("&"))
     status = "200 OK"
-    headers = [
+    response_headers = [
         ('Content-Type','text/plain')
+        ('Content-Length', str(len(data)))
     ]
-    start_response(status,headers)
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    return iter(body)
+    start_response(status,response_headers)
+    return iter ([data.encode('utf-8')])
     #Ввернуть заголовки, только хз как#
 
