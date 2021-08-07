4ea04588762db40a2bda6b3ea9a22966f2a90b11
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-07 11:06:12 +0300

    Start 2.89

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo /etc/init.d/gunicorn restart
