da98914260be2a86dc5a5de7c6952847c484f56a
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-07 10:29:24 +0300

    Start 2.93

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
-def application(env, start_response):
-
-    s1 = []
-    s2 =  env['QUERY_STRING'].split('&');
-
-    for s in s2:
-        s+= '\n'
-        s1.append(s.encode('utf-8'))
-
-    start_response('200 OK', [('Content-Type','text/plain')])
-    return s1
+def test(env, start_response):
+    status = '200 OK'
+    headers = [
+        ('Contnt-Type','text/plain')
+    ]
+    body= [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers)
+    return (body)
