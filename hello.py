5c08c46f750d71695d52c38d4de87728da50eb68
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-07 11:03:06 +0300

    Start 2.90

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-from re import sub
-
 def app(environ, start_response):
-    output = sub('&', '\n', environ.get('QUERY_STRING', ''))
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return iter([str.encode(output)])
+    """Simplest possible application object"""
+    data = "\n".join(environ.get('QUERY_STRING').split("&"))
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+    ]
+    start_response(status, response_headers)
+    return iter ([data.encode('utf-8')])
+    
