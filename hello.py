4b6c7c2ff18f01406a944ffe34da9af4448e5e6a
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-07 11:14:53 +0300

    works

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ def app(environ, start_response):
         ('Content-type', 'text/plain'),
     ]
     start_response(status, response_headers)
-    return iter ([data])
+    return iter ([data.encode('utf-8')])
     
