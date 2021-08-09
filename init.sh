192c40e89b9f51f533a163351f6a39471bec5ead
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-09 12:57:40 +0300

    Start3.2

--- init.sh
+++ init.sh
@@ -1,14 +1,7 @@
-sudo pip3 install virtualenv
-sudo pip3 install pathlib2
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install django
+sudo rm -rf /etc/nginx/sites-enabled/default
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
 
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
-
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
