a768bf068a6d01c9e8806c200436f7cd948e2a84
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-12 11:00:40 +0300

    Start3.5.0

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'stepic_web': BASE_DIR / 'db.sql',
     }
 }
 
--- init.sh
+++ init.sh
@@ -1,8 +1,3 @@
-sudo pip3 install virtualenv
-sudo pip3 install pathlib2
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install django
 
 sudo rm -rf /etc/nginx/sites-enabled/default
 
@@ -11,3 +6,12 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.
 sudo /etc/init.d/nginx restart
 
 gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
+
+sudo python3 -m pip install mysqlclient
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+python3 manage.py makemigrations qa
+python3 manage.py migrate
