0410f6a056ae42b9231ea72ad89839030f3400e8
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-12 15:48:10 +0300

    Start3.5.7

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+  def new(self):
+    return self.order_by('-added_at')
+  def popular(self):
+    return self.order_by('-rating')
+
 class Question(models.Model):
+    object = QuestionManager()
     title = models.CharField(default='', max_length=1024)
     text = models.TextField(default='')
     added_at = models.DateField(null=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User,related_name="q_to_likes")
-
+    
     def __str__(self):
       return self.title
     
     def get_url(self):
       return "/questions/{}/".format(self.id)
-
-class QuestionManager(models.Manager):
-  def new(self):
-    return self.order_by('-added_at')
-  def popular(self):
-    return self.order_by('-rating')
 
 class Answer(models.Model):
     text = models.TextField(default='')
