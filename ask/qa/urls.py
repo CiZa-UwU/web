23d025a5c6f0f9e80cc83193be27760a5b8d9a22
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 19:58:26 +0300

    Задание 2.5 Попытка 5 (я устал()

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
+from django.contrib import admin
 
-urlpatterns = patterns('qa.views',
-	url(r'^$', 'index', name='index'),
-	url(r'^login/', 'login', name='login'),
-	url(r'^signup/', 'signup', name='signup'),
-	url(r'^question/(?P<quest_id>[0-9]+)/$', 'question', name='question'),
-	url(r'^ask/', 'ask', name='ask'),
-	url(r'^answer/', 'answer', name='answer'),
-	url(r'^popular/', 'popular', name='popular'),	
-	url(r'^new/', 'test', name='new'),	
-)
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^popular/.*$', views.popular, name='popular'),    
+    url(r'^answer/.*$', views.answer, name='answer'),   
+    url(r'^new/.*$', views.test),
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),    
+]
