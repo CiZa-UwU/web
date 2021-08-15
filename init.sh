d57b1b71e3db4d779eeb2c970a25e3549d6df759
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 20:50:26 +0300

    Задание 2.5 Попытка 5 (я устал()

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
-sudo -s ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
-sudo -s rm /etc/nginx/sites-enabled/default
-sudo -s /etc/init.d/nginx restart
-
 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE stepic_web;"
