71b1ccdfbfbc4d3c7091bbade26bf1b0220df47f
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 19:22:38 +0300

    Задание 2.5 Попытка 5 (откат)

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,54 +1,29 @@
-from django.http import HttpResponse 
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from qa.models import Question
+from django.db import models
+from django.contrib.auth.models import User
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')  
 
-def question(request,num,title,text):
-    try:
-        q = Question.objects.get(id=num)
-    except Question.DoesNotExist:
-        raise Http404
-    return render(request,'question.html',
-    {
-    'question':q, 
-    'title':title,
-    'text':text
-    })
+# Create your models here.
 
-def index(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-id')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-    return render(request,'list.html',{
-        'title':'latest',
-        'paginator': paginator,
-        'questions': page.object_list,
-        'page': page,
-    })
+class Question(models.Model):
+    title = models.CharField(default="", max_length=1024)
+    text = models.TextField(default="")
+    added_at = models.DateField(null=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="q_to_likes")
 
-def popular(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-id')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-    return render(request,'list.html',{
-        'title':'popular',
-        'paginator': paginator,
-        'questions': page.object_list,
-        'page': page,
-    }) 
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
+
+
+class Answer(models.Model):
+    text = models.TextField(default="")
+    added_at = models.DateField(null=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
