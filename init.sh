a597dfe3a36e4540c5d7df6317e33cb48a535341
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 20:43:29 +0300

    Задание 2.5 Попытка 5 (я устал()

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+sudo -s ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
+sudo -s rm /etc/nginx/sites-enabled/default
+sudo -s /etc/init.d/nginx restart
+
+
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE stepic_web;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
