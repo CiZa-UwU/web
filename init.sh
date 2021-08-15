7ba0ade225a2f67a0145bed97279c9843bc15e48
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 13:32:23 +0300

    Задание 2.5 Попытка 3 (Не работает gunicorn)

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-sudo /etc/init.d/gunicorn restart
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
