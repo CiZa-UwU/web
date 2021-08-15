f962d5c8d1173e8d4bf10a9b5b68e57bb8856152
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 19:45:33 +0300

    Задание 2.5 Попытка 5 (я устал()

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,54 +1,66 @@
-from django.http import HttpResponse 
-from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.core.paginator import Paginator
-from qa.models import Question
+from django.http import HttpResponse, Http404
+from qa.models import Question, Answer
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')  
+    return HttpResponse('OK')
 
-def question(request,num,title,text):
-    try:
-        q = Question.objects.get(id=num)
-    except Question.DoesNotExist:
-        raise Http404
-    return render(request,'question.html',
-    {
-    'question':q, 
-    'title':title,
-    'text':text
-    })
+def index(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-id')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	page = paginator.page(page)
+	return render(request, 'questionList.html', {
+		'title' : 'qwests and answers',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
+	
 
-def index(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-id')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-    return render(request,'list.html',{
-        'title':'latest',
-        'paginator': paginator,
-        'questions': page.object_list,
-        'page': page,
-    })
+def popular(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-likes')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	page = paginator.page(page)
+	return render(request, 'questionList.html', {
+		'title' : 'popular quests',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
 
-def popular(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-id')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-    return render(request,'list.html',{
-        'title':'popular',
-        'paginator': paginator,
-        'questions': page.object_list,
-        'page': page,
-    }) 
+def question(request, quest_id) :
+	try :
+		quest = Question.objects.get(id = quest_id)
+	except Question.DoesNotExist :
+		raise Http404
+	answers = Answer.objects.all().filter(question = quest)
+	
+	title = 'qwest ' + quest_id
+
+	return render(request, 'question.html', {
+		'title' : title,
+		'question' : quest,
+		'list' : answers,
+	})
