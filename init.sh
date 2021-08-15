a3fb03ae70d6843fce29cd70929bd3ebae482516
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 14:53:12 +0300

    Задание 2.5 Попытка 4 (пробую запусить через runserver)

--- init.sh
+++ init.sh
@@ -1,11 +1,8 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
 sudo apt update
 sudo apt install python3.5 -y
 sudo apt install python3.5-dev -y
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo python3 -m pip install gunicorn
 sudo python3 -m pip install django==2.0
 sudo python3 -m pip install mysqlclient
 sudo /etc/init.d/mysql start
