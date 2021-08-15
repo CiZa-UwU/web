9551b69c1a06aecbdac4beb11d9ce8eb40ac6e8c
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-15 19:20:00 +0300

    Задание 2.5 Попытка 5 (откат)

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,16 +1,23 @@
+from django.http import HttpResponse 
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from qa.models import Question
 
-def question(request, num,):
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')  
+
+def question(request,num,title,text):
     try:
         q = Question.objects.get(id=num)
     except Question.DoesNotExist:
         raise Http404
-    return render(request, 'question.html', {'question': q,
-                                              })
-
+    return render(request,'question.html',
+    {
+    'question':q, 
+    'title':title,
+    'text':text
+    })
 
 def index(request):
     try:
@@ -22,14 +29,12 @@ def index(request):
     questions = Question.objects.all().order_by('-id')
     paginator = Paginator(questions, 10)
     page = paginator.page(page)
-
-    return render(request, 'list.html',
-                  {'title': 'Latest',
-                   'paginator': paginator,
-                   'questions': page.object_list,
-                   'page': page,
-                    })
-
+    return render(request,'list.html',{
+        'title':'latest',
+        'paginator': paginator,
+        'questions': page.object_list,
+        'page': page,
+    })
 
 def popular(request):
     try:
@@ -38,13 +43,12 @@ def popular(request):
         page = 1
     except TypeError:
         page = 1
-    questions = Question.objects.all().order_by('-rating')
+    questions = Question.objects.all().order_by('-id')
     paginator = Paginator(questions, 10)
     page = paginator.page(page)
-
-    return render(request, 'list.html',
-                  {'title': 'Popular',
-                   'paginator': paginator,
-                   'questions': page.object_list,
-                   'page': page,
-                    })
+    return render(request,'list.html',{
+        'title':'popular',
+        'paginator': paginator,
+        'questions': page.object_list,
+        'page': page,
+    }) 
