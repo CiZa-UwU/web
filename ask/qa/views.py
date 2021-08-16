0249a109769f3cf665296df730a5048f0ce0a958
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-16 11:21:28 +0300

    Задание 2.5 Попытка 6 (фильтруем главную страницу)

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,66 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, Http404
+from django.core.paginator import Paginator
+
 from qa.models import Question, Answer
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+# Create your views here.
 
-def index(request) :
-	pageLimit = 10
-	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
-	qwests = Question.objects.all().order_by('-added_at')
-	
-	from django.core.paginator import Paginator
-	
-	page = request.GET.get('page') or 1
-	try :
-		page = int(page)
-	except ValueError :
-		page = 1
-	paginator = Paginator(qwests, pageLimit)
-	paginator.baseurl = '/?page='
-	page = paginator.page(page)
-	return render(request, 'questionlist.html', {
-		'title' : 'qwests and answers',
-		'list' : page.object_list,
-		'paginator' : paginator, 
-		'page' : page,
-	})
-	
+def question(request, num,):
+    try:
+        q = Question.objects.get(id=num)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'question.html', {'question': q,
+                                              })
 
-def popular(request) :
-	pageLimit = 10
-	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
-	qwests = Question.objects.all().order_by('-raiting')
-	
-	from django.core.paginator import Paginator
-	
-	page = request.GET.get('page') or 1
-	try :
-		page = int(page)
-	except ValueError :
-		page = 1
-	paginator = Paginator(qwests, pageLimit)
-	paginator.baseurl = '/?page='
-	page = paginator.page(page)
-	return render(request, 'questionlist.html', {
-		'title' : 'popular quests',
-		'list' : page.object_list,
-		'paginator' : paginator, 
-		'page' : page,
-	})
 
-def question(request, quest_id) :
-	try :
-		quest = Question.objects.get(id = quest_id)
-	except Question.DoesNotExist :
-		raise Http404
-	answers = Answer.objects.all().filter(question = quest)
-	
-	title = 'qwest ' + quest_id
+def index(request,**args):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-addet_at')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
 
-	return render(request, 'question.html', {
-		'title' : title,
-		'question' : quest,
-		'list' : answers,
-	})
+    return render(request, 'index.html',
+                  {'title': 'Latest',
+                   'paginator': paginator,
+                   'questions': page.object_list,
+                   'page': page,
+				  })
+
+def popular(request,**args):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-rating')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
+
+    return render(request, 'index.html',
+                  {'title': 'Popular',
+                   'paginator': paginator,
+                   'questions': page.object_list,
+                   'page': page,
+                    })
