c2a342013d935a16eb6b4c49dc6978138ae77559
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2021-08-16 12:19:43 +0300

    Задание 2.5 Попытка 6 (создаем поля Answers)

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^popular/.*$', views.popular, name='popular'),    
-    url(r'^new/.*$', views.test),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),    
 ]
